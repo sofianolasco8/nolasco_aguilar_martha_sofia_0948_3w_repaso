9eeacdc3a478942982a5b213cfbd29251f626f20
sofianolasco8 <a.23308051140948@cbtis128.edu.mx>
2024-11-07 22:01:34 -0600

    Add files via upload

---
+++ pr_11_nolasco_aguilar_martha_sofia_0948_3W.py
@@ -0,0 +1,9 @@
+print(" ")
+print("nolasco_aguilar-martha_sofia_0948_3W")
+print(" ")
+f = "banana" # dando valor a la variable f
+
+for caracter in f: # utlizando el bucle for
+    print(caracter) # print imprime caracter
+
+print(" ") # print imprime espacio
---
+++ pr_12_nolasco_aguilar_martha_sofia_0948_3W.py
@@ -0,0 +1,12 @@
+print(" ")
+print("nolasco_aguilar-martha_sofia_0948_3W")
+print(" ")
+
+for i in range(1, 6): # utiliza bucle for
+    print('*' * i) # imprime 
+
+
+for i in range(4, 0, -1):   # utiliza bucle for
+    print('*' * i) # imprime
+
+print(" ") 
